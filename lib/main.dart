7783ceb4b185dd9afe4a5cfa288ea3f1a8c6b1ab
nischal08 <nischal.karanjit@gmail.com>
2020-09-14 18:20:24 +0545

    Making the dice image change reactively

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var leftDiceNumber = 5;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              // flex: 2,
+              child: FlatButton(
+            onPressed: () {
+              print('Left button got pressed');
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          )),
+          Expanded(
+            // flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                print("right btn pressed");
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
